4ff2cb5e46c7e24b37138564f45ddab6aa1d447b
Ismael Nieto <ismael.nieto@gmail.com>
2021-05-07 13:39:17 +0200

    correction

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,5 +2,4 @@
 
 module.exports = {
   uri: "mongodb://localhost:27017",
-  //uri:  "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.nmckh.mongodb.net/test?authSource=admin&replicaSet=atlas-gnu9co-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
 };
