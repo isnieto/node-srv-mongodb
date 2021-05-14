806235df3f62632784f4729023fabacb17eb22a9
Ismael Nieto <ismael.nieto@gmail.com>
2021-05-14 09:40:09 +0200

    db enviroment variables modified

--- src/config/db.connection.js
+++ src/config/db.connection.js
@@ -1,13 +1,17 @@
-// Create database connection
+// Create database connection and  import enviroment variables
 const mongoose = require("mongoose");
-const mongodb_uri = require("./db.config");
-console.log(mongodb_uri)
-const connection = mongoose.connect(mongodb_uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true
-})
-  .then(db => console.log("Connection established with " + mongodb_uri ))
-  .catch(err => console.log(err))
+const dotenv = require("dotenv");
+dotenv.config();
+
+const connection = mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((db) =>
+    console.log("Connection established with " + process.env.MONGO_URI)
+  )
+  .catch((err) => console.log(err));
 
 module.exports = connection;
